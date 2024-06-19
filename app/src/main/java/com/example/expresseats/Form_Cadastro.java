941a0fcbbf3d819dfd470d4c6823d7886837cba7
Pablo <pablogonzaga14@gmail.com>
2024-06-18 21:49:47 -0300

    final screen login and start screen registration

--- app/src/main/java/com/example/expresseats/Form_Cadastro.java
+++ app/src/main/java/com/example/expresseats/Form_Cadastro.java
@@ -1,9 +1,6 @@
 package com.example.expresseats;
 
-import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -11,33 +8,17 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class Form_Login extends AppCompatActivity {
-
-    private TextView txtCriarConta;
-
+public class Form_Cadastro extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_form_login);
+        setContentView(R.layout.activity_form_cadastro);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
-        IniciarComponentes();
-        txtCriarConta.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(Form_Login.this,Form_Cadastro.class);
-                startActivity(intent);
-            }
-
-        });
-    }
-
-    public void IniciarComponentes(){
-        txtCriarConta =findViewById(R.id.txtCriarConta);
     }
 }
